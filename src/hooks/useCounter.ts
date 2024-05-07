eaa4f842edf480a9c7d67bbf6c62db14ffb57949
Andres Rivero <andres19rivero@gmail.com>
2024-05-07 12:58:46 -0400

    feat: use counter hook along with fetch hook to show pokemon

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -3,11 +3,11 @@ import { useState } from "react";
 export const useCounter = (initialValue: number = 10) => {
   const [counter, setCounter] = useState(initialValue);
 
-  const increment = (value: number) => {
+  const increment = (value: number = 1) => {
     setCounter(counter + value);
   };
 
-  const decrement = (value: number) => {
+  const decrement = (value: number = 1) => {
     // if (counter === 0) return;
 
     setCounter(counter - value);
