4ed677e5bbbe4534ee0233858e1e5347a52abf8e
Andres Rivero <andres19rivero@gmail.com>
2024-05-07 11:23:44 -0400

    feat(useFetch): parametrize & consume custom hook

--- src/03-examples/MultipleCustomHooks.tsx
+++ src/03-examples/MultipleCustomHooks.tsx
@@ -1,10 +1,18 @@
-import { useFetch } from "../hooks"
-
+import { useFetch } from "../hooks";
 
 export const MultipleCustomHooks = () => {
-  useFetch();
+  const { data, hasError, isLoading } = useFetch(
+    "https://pokeapi.co/api/v2/pokemon/1",
+  );
 
   return (
-    <h1>MultipleCustomHooks</h1>
-  )
-}
+    <>
+      <h1>Pokemon Information</h1>
+      <hr />
+
+      {isLoading && <p>Loading...</p>}
+
+      <h2>{data?.name}</h2>
+    </>
+  );
+};
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = () => {
-  const [state, setState] = useState({
+export interface fetchState {
+  data: any;
+  isLoading: boolean;
+  hasError: boolean;
+  error: any;
+}
+
+export const useFetch = (url: string) => {
+  const [state, setState] = useState<fetchState>({
     data: null,
     isLoading: true,
     hasError: false,
@@ -10,12 +17,44 @@ export const useFetch = () => {
 
   useEffect(() => {
     getFetch();
-  }, []);
+  }, [url]);
+
+  const setLoadingState = () => {
+    setState({
+      data: null,
+      isLoading: true,
+      hasError: false,
+      error: null,
+    });
+  };
 
   const getFetch = async () => {
-    const resp = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    setLoadingState();
+    const resp = await fetch(url);
+
+    // sleep
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    if (!resp.ok) {
+      setState({
+        data: null,
+        isLoading: false,
+        hasError: true,
+        error: {
+          code: resp.status,
+          message: resp.statusText,
+        },
+      });
+      return;
+    }
+
     const data = await resp.json();
-    console.log({ data });
+    setState({
+      data,
+      isLoading: false,
+      hasError: false,
+      error: null,
+    });
   };
 
   return {
