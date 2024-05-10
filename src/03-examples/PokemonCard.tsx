009e52567e70f36c83b3b3bbc54887238452b44e
Andres Rivero <andres19rivero@gmail.com>
2024-05-09 23:26:31 -0400

    feat(useLayoutEffect): create Layout component to test hook

--- src/03-examples/PokemonCard.tsx
+++ src/03-examples/PokemonCard.tsx
@@ -1,15 +1,32 @@
-export const PokemonCard = ({ id, name, sprites = [] }) => {
+import { useLayoutEffect, useRef, useState } from "react";
+
+interface IPokemon {
+  id: string;
+  name: string;
+  sprites: string[];
+}
+
+export const PokemonCard = ({ id, name, sprites = [] }: IPokemon) => {
+  const divRef = useRef<HTMLDivElement>(null);
+  const [boxSize, setBoxSize] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
+    const { height, width } = divRef.current.getBoundingClientRect()
+    setBoxSize({ height, width });
+  }, [id]);
+
   return (
     <section style={{ height: 200 }}>
       <h2 className="text-capitalize">
         #{id} - {name}
       </h2>
       {/* Images */}
-      <div>
+      <div ref={divRef}>
         {sprites.map((sprite) => (
           <img key={sprite} src={sprite} alt={name} />
         ))}
       </div>
+      <code>{JSON.stringify(boxSize)}</code>
     </section>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+import { Layout } from "./05-useLayoutEffect/Layout";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
@@ -11,6 +12,6 @@ import { FocusScreen } from "./04-useRef/FocusScreen";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FocusScreen />
+    <Layout />
   </React.StrictMode>,
 );
